627c403c5a7ae7a0a318a447c6f892d3eca1b108
Fawaz Ibraheem <fawazming@gmail.com>
2022-08-10 00:04:29 +0100

    JS 101 Tuts

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// To print out
+// console.log(123);
+
+
+let ageIntro = "My Grandpa's age is ";
+let age = 80;
+let like = false;
+let name = 'Fawaz';
+
+
+console.log("My Grandpa's age is "+age);
+console.log(`He said "My Father's name is `+name+`" `);
+console.log(`Did you like the post? `+like);
+
+age = 16;
+
+console.log("Oh! no My Grandpa's age is "+age);
+// Data Types
+// Number 80 90 100 but without quote
+
+// String e.g "Fawaz", "Sunshine", '300', "hello". String always work with the quote (Single quote'', Double quote"", Back Tick``)
+
+//Boolean is either true or false, 1 or 0;
+
+
+
+// Arithmetic Operator
+let k=5, y=8, z=4;
+let sum = k + y;
+console.log(k,y,z);
+console.log(`Add x + y. Answer is `+ sum);
+
+// Template literal: ` ${variable}`
+console.log(`Modulus means remaider. k which has a value of ${k}  modulo z which has a value of ${z}. Answer is ${k%z}`);
+
+console.log(`Subtract k which has a value of ${k}  from z which has a value of ${z}. Answer is ${z-k}`);
+
+console.log(`Modulus means remaider. k which has a value of ${k}  modulo z which has a value of ${z}. Answer is ${k%z}`);
